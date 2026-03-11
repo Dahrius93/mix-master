745cc0c8723f810635f888b3bb96df72ca5a3650
Dahrius93 <dariolauretta93@gmail.com>
2026-03-11 23:24:31 +0100

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,51 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Cocktail,
+  Error,
+  HomeLayout,
+  Landing,
+  Newsletter,
+} from "./pages";
+
+// router example
+//
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <h2>home page</h2>,
+//   },
+// ]);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/cocktail",
+    element: <Cocktail />,
+  },
+  {
+    path: "/error",
+    element: <Error />,
+  },
+  {
+    path: "/landing",
+    element: <Landing />,
+  },
+  {
+    path: "/newsletter",
+    element: <Newsletter />,
+  },
+]);
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return <router.Provider router={router} />;
 };
 export default App;
