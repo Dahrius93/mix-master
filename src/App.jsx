b09859905d86269e62939204f75980485def3e10
Dahrius93 <dariolauretta93@gmail.com>
2026-03-12 20:56:16 +0100

    pagina error completata

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
+    error: <Error />,
     children: [
       {
         //path: "/landing",
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,29 @@
+import Wrapper from "../assets/wrappers/ErrorPage";
+import { Link, useRouteError } from "react-router-dom";
+import img from "../assets/not-found.svg";
+
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  console.log(error);
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="Not found" />
+          <h3>Ohh!</h3>
+          <p>We can't seem to find the page you're looking for.</p>
+          <Link to="/">Go back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div>
+        <h3>Something went wrong</h3>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
