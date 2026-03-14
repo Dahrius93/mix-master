9d8108c06d6a0c1ce1dc9b4981d036a9bd9b86c8
Dahrius93 <dariolauretta93@gmail.com>
2026-03-14 16:29:37 +0100

    newsletter

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,5 +1,56 @@
 const Newsletter = () => {
-  return <h3>Newsletter</h3>;
+  return (
+    <form className="form">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        our newsletter
+      </h4>
+      {/* name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          className="form-input"
+          defaultValue="john"
+        />
+      </div>
+      {/* lastName */}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          lastName
+        </label>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          className="form-input"
+          defaultValue="smith"
+        />
+      </div>
+      {/* email */}
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          className="form-input"
+          defaultValue="john@example.com"
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: "2rem" }}
+      >
+        Subscribe
+      </button>
+    </form>
+  );
 };
 
 export default Newsletter;
