6b428bfd67ce7c91e7c18c6d2499b70d92005bea
Dahrius93 <dariolauretta93@gmail.com>
2026-03-18 18:53:25 +0100

    utilizzo tastack-query su Landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import {
   About,
@@ -27,6 +29,15 @@ import { action as newsletterAction } from "./pages/Newsletter";
 // │  │  └─────────────┘  │  │
 // │  └───────────────────┘  │
 // └─────────────────────────┘
+
+const queryClient = new QueryClient({
+  // questa è la configurazione di default per tutte le query, in questo caso tutte le query saranno considerate "fresche" per 5 minuti, quindi non verranno refetchate automaticamente se vengono richiamate entro 5 minuti dall'ultima volta che sono state fetchate
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -66,6 +77,12 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
+
 export default App;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,17 +6,37 @@ import SearchForm from "../components/SearchForm";
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
+import { useQuery } from "@tanstack/react-query";
+
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ["search", searchTerm || "all"],
+    queryFn: async () => {
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return response.data.drinks;
+    },
+  };
+};
+
 // eseguito prima di renderizzare il componente
 // ma anche al cambio URL come submit form
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
   const searchTerm = url.searchParams.get("search") || "a";
-  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-  return { drinks: response.data.drinks, searchTerm }; // ritornare sempre qualcosa per evitare undefined
+  // const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`); // lo commento perchè utilizzerò useQuery
+  //return { drinks: response.data.drinks, searchTerm }; // ritornare sempre qualcosa per evitare undefined
+  return { searchTerm }; // ritorno solo searchTerm perchè i dati li gestirò con useQuery all'interno del componente, in questo modo la query sarà refetchata automaticamente al cambio URL senza dover rifare tutta la logica di fetch nella funzione loader
 };
 
 const Landing = () => {
-  const { drinks, searchTerm } = useLoaderData();
+  const { searchTerm } = useLoaderData();
+  const { data: drinks, isLoading } = useQuery(
+    searchCocktailsQuery(searchTerm),
+  );
+  if (isLoading) {
+    return <h4 style={{ textAlign: "center" }}>Loading...</h4>;
+  }
+
   // const data = useLoaderData(); ritorna un oggetto con tutte le proprietà restituite dalla funzione loader, in questo caso { drinks, searchTerm }
   return (
     <>
